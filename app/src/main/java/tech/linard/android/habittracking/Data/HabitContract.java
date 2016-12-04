957671f954c689bebfaf097bec3f88594a966e93
lucas <contato@linard.tech>
2016-12-04 01:28:47 -0200

    INITIAL COMMIT

---
+++ app/src/main/java/tech/linard/android/habittracking/Data/HabitContract.java
@@ -0,0 +1,20 @@
+package tech.linard.android.habittracking.Data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by lucas on 04/12/16.
+ */
+
+public final class HabitContract {
+    private HabitContract() {
+    }
+    public static class HabitEntry implements BaseColumns {
+        // Table defined here;
+        public final static String TABLE_NAME = "habits";
+        public final static String COLUMN_ID = BaseColumns._ID;
+        public final static String COLUMN_ACTION = "action";
+        public final static String COLUMN_DURATION = "duration";
+        public final static String COLUMN_DATE = "date";
+    }
+}
---
+++ app/src/main/java/tech/linard/android/habittracking/MainActivity.java
@@ -0,0 +1,85 @@
+package tech.linard.android.habittracking;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import tech.linard.android.habittracking.Data.HabitDBHelper;
+import tech.linard.android.habittracking.Data.HabitContract.HabitEntry;
+
+public class MainActivity extends AppCompatActivity {
+
+    HabitDBHelper mDBHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        insertDummyData();
+        readDummyData();
+
+
+    }
+
+    private void readDummyData() {
+        mDBHelper = new HabitDBHelper(this);
+        SQLiteDatabase db = mDBHelper.getReadableDatabase();
+
+        // Prepare the select statement;
+        String[] projection = {
+                HabitEntry.COLUMN_ACTION,
+                HabitEntry.COLUMN_DURATION,
+                HabitEntry.COLUMN_DATE
+        };
+
+        Cursor cursor = db.query(
+                HabitEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        int indexAction = cursor.getColumnIndex(HabitEntry.COLUMN_ACTION);
+        int indexDuration = cursor.getColumnIndex(HabitEntry.COLUMN_DURATION);
+        int indexDate = cursor.getColumnIndex(HabitEntry.COLUMN_DATE);
+
+        TextView txtCount = (TextView) findViewById(R.id.txt_count);
+        txtCount.setText(Integer.toString(cursor.getCount()));
+        while (cursor.moveToNext()) {
+            String action = cursor.getString(indexAction);
+            int duration  = cursor.getInt(indexDuration);
+            String date = cursor.getString(indexDate);
+
+            txtCount.append(
+                    "\n"
+                    + action + " - "
+                    + Integer.toString(duration) + " - "
+                    + date
+            );
+        }
+
+
+    }
+
+    private void insertDummyData() {
+        ContentValues values = new ContentValues();
+        values.put(HabitEntry.COLUMN_ACTION, "Android Programming");
+        values.put(HabitEntry.COLUMN_DURATION, 240 ); // minutes
+        values.put(HabitEntry.COLUMN_DATE, "03.12.2016");
+        mDBHelper = new HabitDBHelper(this);
+        SQLiteDatabase db = mDBHelper.getWritableDatabase();
+        long result = db.insert(HabitEntry.TABLE_NAME, null, values);
+        if (result == -1) {
+            Toast.makeText(this, "INSERT UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "INSERT SUCCESSFUL with ID of " + result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
